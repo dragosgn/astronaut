0cc24c7fda45da768c0659f3ee6f5c1f4ed4e9e7
Dragos Nedelcu <dragosgn@gmail.com>
2020-06-07 10:02:23 +0200

    linting shall pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders Powered by React link", () => {
+test('renders Powered by React link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Powered by React/i);
   expect(linkElement).toBeInTheDocument();
